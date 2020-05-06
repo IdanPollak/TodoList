42156fd3eb26b30ae7f0d6673d587f8845585dee
Idan Pollak <idanpol1@gmail.com>
2020-05-06 18:04:42 +0300

    Updates

---
+++ precache-manifest.d787a57169796b472b9941dfc8409a2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5543681c0b35a79fbb39f105dc6f424b",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "832528a9527011ec7371",
+    "url": "/TodoList/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3f1502ca738f7e6e9e8b",
+    "url": "/TodoList/static/js/2.8af7bf61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TodoList/static/js/2.8af7bf61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "832528a9527011ec7371",
+    "url": "/TodoList/static/js/main.ee6c0919.chunk.js"
+  },
+  {
+    "revision": "b7d2fb8a362195bceb86",
+    "url": "/TodoList/static/js/runtime-main.ea69cb18.js"
+  }
+]);
